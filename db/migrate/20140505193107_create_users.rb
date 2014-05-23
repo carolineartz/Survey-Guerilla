6ec98e68063fa8ca05a29b37a9aa0372b60a65db
Tom & msabdeljalil <pair+githop.msabdeljalil@devbootcamp.com>
2014-05-23 14:05:26 -0500

    migrations for 6 tables added

--- db/migrate/20140505193107_create_users.rb
+++ db/migrate/20140505193107_create_users.rb
@@ -2,9 +2,25 @@ class CreateUsers < ActiveRecord::Migration
   def change
   	create_table :users do |t|
   		t.string :email
+      t.string :username, unique: :true
   		t.string :password_hash
 
   		t.timestamps
   	end
   end
 end
+
+#6 tables
+
+# users
+# surveys
+# user_surveys (join table)
+
+# possible_choices
+# questions
+# possible_choices_questions (join table)
+
+# A user has many surveys
+# A survey has many questions
+# A question has many possible_choices
+
