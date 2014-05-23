2d17067f4c841eb0a1ca448699f0cea7647c09b5
Tom & msabdeljalil <pair+githop.msabdeljalil@devbootcamp.com>
2014-05-23 17:46:30 -0500

    users can create a survey with single question, answer, and choice

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,5 +10,12 @@ get '/surveys/new' do
 end
 
 post '/surveys' do
-
+	new_survey = Survey.create(title: params[:title])
+	new_question = Question.create(question_text: params[:question_text], survey_id: new_survey.id)
+	new_choices = Choice.create(choice_text: params[:choice_text], question_id: new_question.id)
+		if new_survey && new_question && new_choices
+			redirect "/users/#{session[:user_id]}"
+		else
+			"whoops"
+		end
 end
