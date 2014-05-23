ac564fca078c2b48a4682ca0938aacaecb826286
Dusty Wilson <dwwilson21@gmail.com>
2014-05-23 16:04:46 -0500

    Seed file working

--- db/migrate/20140523135841_create_survey_responses.rb
+++ db/migrate/20140523135841_create_survey_responses.rb
@@ -1,8 +1,8 @@
 class CreateSurveyResponses < ActiveRecord::Migration
   def change
     create_table :survey_responses do |t|
-      t.integer :participations_id
-      t.integer :choices_id
+      t.integer :participation_id
+      t.integer :choice_id
 
       t.timestamps
     end
