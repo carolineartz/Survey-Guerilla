ac97052ab6bae92f87e5591e67abc4f7ffca46c7
msabdeljalil <msabdeljalil@gmail.com>
2014-05-24 19:43:07 -0500

    Removed extra seeds from the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,56 +6,15 @@ User.create(email: "lovely@example.com", username: "user4", password: "password"
 User.create(email: "sexy@example.com", username: "user5", password: "password")
 User.create(email: "ugly@example.com", username: "user6", password: "password")
 #surveys
-marvel_survey = Survey.create(title: "Marvel Comic Books", user_id: 1)
 Survey.create(title: "StarWars Characters", user_id: 2)
 Survey.create(title: "Favorite Invertibrates", user_id: 3)
 Survey.create(title: "Favorite Pets", user_id: 4)
-# #questions
-# Question.create(question_text: "State your favorite Marvel hero.", survey_id: 1)
-# Question.create(question_text: "Which StarWars character do you relate to most?", survey_id: 2)
-# Question.create(question_text: "State your favorite spineless creature", survey_id: 3)
-# Question.create(question_text: "Dogs or Cats?", survey_id: 4)
-# #the users and the surveys they took
-# Participation.create(user_id: 1, survey_id: 1)
-# Participation.create(user_id: 2, survey_id: 2)
-# Participation.create(user_id: 3, survey_id: 3)
-# Participation.create(user_id: 4, survey_id: 4)
-# Participation.create(user_id: 1, survey_id: 2)
-# #all choices for all questions
-# Choice.create(choice_text: "Spiderman", question_id: 1)
-# Choice.create(choice_text: "Ironman", question_id: 1)
-# Choice.create(choice_text: "Obi-Wan Kenobi", question_id: 2)
-# Choice.create(choice_text: "Han Solo", question_id: 2)
-# Choice.create(choice_text: "Troglobites", question_id: 3)
-# Choice.create(choice_text: "Jelly Fish", question_id: 3)
-# Choice.create(choice_text: "dog", question_id: 4)
-# Choice.create(choice_text: "cat", question_id: 4)
-# #user responses
-# SurveyResponse.create(participation_id: 1, choice_id: 2, survey_id: 1)
-# SurveyResponse.create(participation_id: 2, choice_id: 4, survey_id: 2)
-# SurveyResponse.create(participation_id: 3, choice_id: 6, survey_id: 3)
-# SurveyResponse.create(participation_id: 4, choice_id: 7, survey_id: 4)
-# SurveyResponse.create(participation_id: 2, choice_id: 3, survey_id: 2)
-
-# #testing the results page
-# SurveyResponse.create(participation_id: 1, choice_id: 2, survey_id: 1)
-# SurveyResponse.create(participation_id: 2, choice_id: 3, survey_id: 1)
-# SurveyResponse.create(participation_id: 3, choice_id: 3, survey_id: 1)
-# SurveyResponse.create(participation_id: 4, choice_id: 2, survey_id: 1)
-# SurveyResponse.create(participation_id: 5, choice_id: 3, survey_id: 1)
-# SurveyResponse.create(participation_id: 1, choice_id: 2, survey_id: 1)
-# SurveyResponse.create(participation_id: 2, choice_id: 3, survey_id: 1)
-# SurveyResponse.create(participation_id: 3, choice_id: 3, survey_id: 1)
-# SurveyResponse.create(participation_id: 4, choice_id: 2, survey_id: 1)
-# SurveyResponse.create(participation_id: 5, choice_id: 3, survey_id: 1)
 
 
 
+marvel_survey = Survey.create(title: "Marvel Comic Books", user_id: 1)
 
-
-
-#additional questions
-
+# questions
 q1 = Question.create(question_text: "Who is your favorite Marvel Villian?", survey_id: 1)
   q1c1 = Choice.create(choice_text: "Mandarin", question_id: q1.id)
   q1c2 = Choice.create(choice_text: "Magneto", question_id: q1.id)
